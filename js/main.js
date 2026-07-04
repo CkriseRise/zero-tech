bdd265c0f633a9d195b87e07d691567f59c89f03
Ckrise <2397589964@qq.com>
2026-07-04 19:09:01 +0800

    将四个js文件模块化

---
+++ js/main.js
@@ -0,0 +1,7 @@
+import { initNav } from "./nav.js";
+import { initCardsAnim } from "./cards.js";
+import { initScoreAnim } from "./score.js";
+// 大括号是必须加的
+initCardsAnim()
+initScoreAnim()
+initNav()
