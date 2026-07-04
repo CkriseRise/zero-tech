4e15c062fa1af9bcec32e86bb74c797ed8cd7422
Ckrise <2397589964@qq.com>
2026-07-04 18:54:49 +0800

    模块化开发

--- Code/motivate.js
+++
@@ -1,7 +0,0 @@
-let i = 1;
-let j = 1;
-function changeText() {
-  document.getElementById('msg').textContent = i++ % 2 == 0 ? '你刚刚触发了一段 JavaScript。' : '这是我的第一个网页，现在它有了一点样式。';
-  document.querySelector('button').textContent = j++ % 2 == 0 ? '点我还原' : '点我试试';
-  
-}
